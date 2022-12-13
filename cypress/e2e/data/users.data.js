187499c0f26a8acbbe2d5f27411976ebd080be4c
Rogerio Jones <rogonjones@gmail.com>
2022-12-13 00:42:21 -0500

    Added data and faker

---
+++ cypress/e2e/data/users.data.js
@@ -0,0 +1,20 @@
+module.exports = {
+    valid: {
+        email: 'testing1@testmail.com',
+        password: 'Password123',
+        full_name: 'Test User',
+        streetAddress:'2 test street',
+        city: 'Test City',
+        country:'United States',
+        state: 'New York',
+        zipcode: '10001',
+        card: '4242 4242 4242 4242',
+        cvv:'123',
+        cardDate:'12/30',
+        shipment: 'FREE PONY EXPRESS DELIVERY!'
+    },
+    invalid: {
+        email:'invalid@testmail.com',
+        password: 'Password123',
+    },
+}
