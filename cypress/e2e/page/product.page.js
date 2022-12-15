60e2ff4d36cfc06dbc5cda3b2eac17657924f40f
Rogerio Jones <rogonjones@gmail.com>
2022-12-15 00:26:38 -0500

    Finish Add-to-cart

--- cypress/e2e/page/product.page.js
+++ cypress/e2e/page/product.page.js
@@ -6,6 +6,10 @@ class Products {
     get productPriceTags() { return ('.chakra-text.css-0') }
     get productTile() { return ('.chakra-stack.css-uaqjf') }
     get productName() { return ('.chakra-text.css-1n64n71') }
+
+    get backToProductsBtn() { return ('.chakra-heading.css-18j379d') }
+    get increaseProductAmountBtn() { return ('#product-increase') }
+    get decreaseProductAmountBtn() { return ('#product-decrease') }
     
 
     getProductId(num) { 
--- cypress/e2e/test/add-to-cart.cy.js
+++ cypress/e2e/test/add-to-cart.cy.js
@@ -1,7 +1,9 @@
-import { faker } from '@faker-js/faker'
 import Auth from '../page/auth.page'
+import Product from '../page/product.page'
+import Cart from '../page/cart.page'
+import products from '../data/products.data'
 import user from '../data/users.data'
-describe('Authentication', () => {
+describe('Add-to-cart', () => {
     beforeEach(() => {
         cy.visit('/')
         cy.get(Auth.signInOrRegisterBtn).click();
@@ -18,8 +20,76 @@ describe('Authentication', () => {
     })
 
     it('Verify that a user can add a single item to the cart', () => {
+      cy.get(Product.getProductId(0))
+      .find(Product.productImgs)
+      .should('be.visible')
+      .click()
+
+      cy.get(Product.backToProductsBtn).should('be.visible')
+      cy.get(Product.addToCartBtns).click()
+      cy.get(Cart.cartCheckoutBtn).should('be.visible')
+
+      cy.get(Cart.itemInCartName).eq(0).should('have.text', products.products[0].name)
 
     })
 
+    it('Verify that a user can add multiple items to cart', () => {
+      cy.get(Product.getProductId(0))
+      .find(Product.productImgs)
+      .should('be.visible')
+      .click()
+
+      cy.get(Product.backToProductsBtn).should('be.visible')
+      cy.get(Product.addToCartBtns).click()
+      cy.get(Cart.cartCheckoutBtn).should('be.visible')
+
+      cy.get(Cart.cartBackBtn).click()
+      cy.get(Product.backToProductsBtn).click()
+
+      cy.get(Product.getProductId(1))
+      .find(Product.productImgs)
+      .should('be.visible')
+      .click()
+
+      cy.get(Product.backToProductsBtn).should('be.visible')
+      cy.get(Product.addToCartBtns).click()
+
+      cy.get(Cart.itemInCartName).eq(0).should('have.text', products.products[1].name)
+      cy.get(Cart.itemInCartName).eq(1).should('have.text', products.products[0].name)
+      
+    })
+
+    it('Verify that a user is able to change the amount of an item they can add to cart', () => {
+      cy.get(Product.getProductId(0))
+      .find(Product.productImgs)
+      .should('be.visible')
+      .click()
+
+      cy.get(Product.backToProductsBtn).should('be.visible')
+      cy.get(Product.increaseProductAmountBtn).click()
+      cy.get(Product.increaseProductAmountBtn).click()
+      cy.get(Product.addToCartBtns).click()
+
+      cy.get(Cart.quantityInCart).should('have.text', '3')
+
+    })
+    
+    it('Verify that the total in the cart updates with each item added', () => {
+      cy.get(Product.getProductId(0))
+      .find(Product.productImgs)
+      .should('be.visible')
+      .click()
+
+      cy.get(Product.backToProductsBtn).should('be.visible')
+      cy.get(Product.addToCartBtns).click()
+      cy.get(Cart.cartCheckoutBtn).should('be.visible')
+
+      cy.get(Cart.cartBackBtn).click()
+      cy.get(Product.addToCartBtns).click()
+
+      cy.get(Cart.cartTotal)
+      .should('be.visible')
+      .contains(`${products.products[0].price+products.products[0].price}`)
+    })
 })
 
