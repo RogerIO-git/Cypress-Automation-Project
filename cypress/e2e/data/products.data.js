177257f197c83a38f96691e233e87d0274e12144
Rogerio Jones <rogonjones@gmail.com>
2022-12-17 04:09:58 -0500

    Finish Filter

--- cypress/e2e/data/products.data.js
+++ cypress/e2e/data/products.data.js
@@ -1,4 +1,10 @@
 module.exports = {
+    filter: {
+        'A to Z':'aToZ',
+        'Z to A':'zToA',
+        'Low to High':'lowToHigh',
+        'High to Low':'highToLow',
+    },
     products: [
         {
             name: ' Quality Fitted Hat ',
@@ -9,72 +15,128 @@ module.exports = {
         {
             name: ' Quality Trucker Hat ',
             description: ' Croogo Quick Dry Sports Hat UPF 50+ Baseball Cap Embroidered Lightweight Outdoor UV Protection Sun Hats for Men Women ',
-            price: 24
+            price: 24,
+            tag: ' hat '
         },
         {
-            name: 'Quality Mousepad',
-            description: 'JIALONG New Upgraded Version Large Mouse Pad Desk Mat Comfortable Mousepad with Personalized Design Extended Size 35.4 X 15.7X 0.12 inches for Laptop, Computer and PC ',
-            price: 20
+            name: ' Quality Mousepad ',
+            description: ' JIALONG New Upgraded Version Large Mouse Pad Desk Mat Comfortable Mousepad with Personalized Design Extended Size 35.4 X 15.7X 0.12 inches for Laptop, Computer and PC ',
+            price: 20,
+            tag: ' computer '
         },
         {
-            name: 'Quality Mug',
-            description: 'Wampumtuk Pack of 2 Company Policies Boss Employee Work Office 11 Ounces Funny Coffee Mug',
-            price: 15
+            name: ' Quality Mug ',
+            description: ' Wampumtuk Pack of 2 Company Policies Boss Employee Work Office 11 Ounces Funny Coffee Mug ',
+            price: 15,
+            tag: ' cup '
         },
         {
-            name: 'Quality Pillow',
-            description: 'Cooling Bed Pillows for Sleeping 2 Pack Shredded Memory Foam Pillows Adjustable Cool BAMBOO Pillow for Side Back Stomach Sleepers - Luxury Gel Pillows Queen Size Set of 2 with Washable Removable Cover',
-            price: 20
+            name: ' Quality Pillow ',
+            description: ' Cooling Bed Pillows for Sleeping 2 Pack Shredded Memory Foam Pillows Adjustable Cool BAMBOO Pillow for Side Back Stomach Sleepers - Luxury Gel Pillows Queen Size Set of 2 with Washable Removable Cover ',
+            price: 20,
+            tag: ' pillow '
         },
         {
-            name: 'Quality Hooded Sweatshirt',
-            description: "Hanes Men's Sweatshirt, EcoSmart Fleece Crewneck Sweatshirt, Cotton-Blend Fleece Sweatshirt, Plush Fleece Pullover Sweatshirt",
-            price: 50
+            name: ' Quality Hooded Sweatshirt ',
+            description: " Hanes Men's Sweatshirt, EcoSmart Fleece Crewneck Sweatshirt, Cotton-Blend Fleece Sweatshirt, Plush Fleece Pullover Sweatshirt ",
+            price: 50,
+            tag: ' shirt '
         },
         {
-            name: 'Quality Sweatshirt',
-            description: "Hanes Men's Sweatshirt, EcoSmart Fleece Crewneck Sweatshirt, Cotton-Blend Fleece Sweatshirt, Plush Fleece Pullover Sweatshirt",
-            price: 40
+            name: ' Quality Sweatshirt ',
+            description: " Hanes Men's Sweatshirt, EcoSmart Fleece Crewneck Sweatshirt, Cotton-Blend Fleece Sweatshirt, Plush Fleece Pullover Sweatshirt ",
+            price: 40,
+            tag: ' shirt '
         },
         {
-            name: 'Quality Kids Tshirt',
+            name: ' Quality Kids Tshirt ',
             description: "",
-            price: 15
+            price: 15,
+            tag: ' shirt '
         },
         {
-            name: 'Quality Tshirt',
-            description: "5 Pack Boys Athletic Shirts, Youth Activewear Dry Fit Tshirts for Kids, Short Sleeve Tees, Bulk Athletic Performance Clothing",
-            price: 20
+            name: ' Quality Tshirt ',
+            description: " 5 Pack Boys Athletic Shirts, Youth Activewear Dry Fit Tshirts for Kids, Short Sleeve Tees, Bulk Athletic Performance Clothing ",
+            price: 20,
+            tag: ' shirt '
         },
         {
-            name: 'Quality Vneck',
+            name: ' Quality Vneck ',
             description: "",
-            price: 20
+            price: 20,
+            tag: ' shirt '
         },
         {
-            name: 'Quality Pink Pants',
-            description: "Kingsted T-Shirts for Men Pack - Royally Comfortable - Soft & Fresh Cotton Blend - Well-Crafted Classic Tee",
-            price: 44
+            name: ' Quality Pink Pants ',
+            description: " Kingsted T-Shirts for Men Pack - Royally Comfortable - Soft & Fresh Cotton Blend - Well-Crafted Classic Tee ",
+            price: 44,
+            tag: ' pants '
         },
         {
-            name: 'Quality Cargo Pants',
-            description: "HIJEWE Men's Military Cargo Pants Casual Lightweight Work Pants Tactical Wild Army Combat Ripstop Trousers with 6 Pockets",
-            price: 17
+            name: ' Quality Cargo Pants ',
+            description: " HIJEWE Men's Military Cargo Pants Casual Lightweight Work Pants Tactical Wild Army Combat Ripstop Trousers with 6 Pockets ",
+            price: 17,
+            tag: ' pants '
         },
         {
-            name: 'Quality Jeans Pants',
+            name: ' Quality Jeans Pants ',
             description: "",
-            price: 28
+            price: 28,
+            tag: ' pants '
         },
         {
-            name: 'Quality Blue Shoes',
-            description: "Epic Step Sneakers for Women Fashion Sneakers Tennis Shoes Women Sneakers Tenis para Mujeres Womens Shoe Sneakers Women's Sneakers",
-            price: 20
+            name: ' Quality Blue Shoes ',
+            description: " Epic Step Sneakers for Women Fashion Sneakers Tennis Shoes Women Sneakers Tenis para Mujeres Womens Shoe Sneakers Women's Sneakers ",
+            price: 20,
+            tag: ' shoes '
         },
         {
-            name: 'Quality Stylish Shoes',
-            description: "Skechers Men's Moreno Canvas Oxford Shoe",
-            price: 20
+            name: ' Quality Stylish Shoes ',
+            description: " Skechers Men's Moreno Canvas Oxford Shoe ",
+            price: 20,
+            tag: ' shoes '
+        },
+        {
+            name: ' Quality Heal Shoes ',
+            description: " Badgley Mischka Women's Everafter Heeled Sandal ",
+            price: 20,
+            tag: ' shoes '
+        },
+        {
+            name: ' Red Couch ',
+            description: " ZINUS Mikhail Sofa Couch / Ruby Red Sofa / Button Tufted Cushions / Easy, Tool-Free Assembly ",
+            price: 320,
+            tag: ' couch '
+        },
+        {
+            name: ' White Couch ',
+            description: " Jummico Convertible Sectional Sofa Couch, Sofa Set for Living Room with 4 Throw Pillows, U-Shaped Modern Minimalist Fabric Modular Sofa with Double Chaise (White) ",
+            price: 234,
+            tag: ' couch '
+        },
+        {
+            name: ' Gray Couch ',
+            description: " Esright 88.6” Convertible Sectional Sofa Couch with Ottoman, Modern Tufted Fabric L-Shaped Couch with Reversible Chaise, Suitable for Office,Living Room and Hotel Lobby, Gray ",
+            price: 550,
+            tag: ' couch '
+        },
+        {
+            name: ' Mackbook Pro ',
+            description: ' 2022 HP 17.3" FHD IPS Laptop, 11th Gen 4-Core Intel Core i5-1135G7(Beats i7-1065G7), Iris Xe Graphics, 16GB 3200Mhz DDR4 RAM, 512GB SSD, Windows 11 S, Silver ',
+            price: 2000,
+            tag: ' laptop '
+        },
+        {
+            name: ' Dell Laptop ',
+            description: ' 2022 Newest Dell Inspiron 15.6" FHD Touchscreen Laptop, Intel 10th Generation Core i5-1035G1(Up to 3.60GHz, Beat i7-8550U), 16GB Memory, 512GB PCIe SSD, Intel UHD Graphics, WiFi, Webcam, HDMI, Win11 S ',
+            price: 1000,
+            tag: ' laptop '
+        },
+        {
+            name: ' HP Laptop ',
+            description: ' 2022 Newest HP 14" FHD Laptop, NonTouch Display, AMD Ryzen3 3250U (up to 3.5 GHz), 8GB RAM, 256GB SSD, Ethernet, Webcam, WiFi, Bluetooth, HDMI,Type-C, Win10 Home ',
+            price: 680,
+            tag: ' laptop '
         },
     ],
 }
